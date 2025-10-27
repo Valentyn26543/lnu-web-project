8886f2d153e710b65ea080c582a17a81dea9cc5a
Valentyn26543 <149696068+Valentyn26543@users.noreply.github.com>
2025-10-27 16:08:43 +0200

    Conect MongoDB, create User model, done register and login on backend, initial CRA for frontend

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,40 @@
 import express from "express";
+import mongoose from "mongoose";
+import "dotenv/config";
+
+import { registerValidation, loginValidation } from "./validations.js";
+import { handleValidationErrors } from "./utils/index.js";
+import { UserController } from "./controllers/index.js";
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("DB ok");
+  })
+  .catch((err) => {
+    console.log("DB error", err);
+  });
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Server OK")
-})
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+
+
+app.get("/", (req, res) => {
+  res.send("Server OK");
+});
 
 app.listen(4444, (err) => {
   if (err) {
---
+++ backend/validations.js
@@ -0,0 +1,14 @@
+import { body } from "express-validator";
+
+export const loginValidation = [
+    body("email", "Неправильний формат пошти").isEmail(),
+    body("password", "Пароль має бути мінімум 5 символів").isLength({ min: 5 }),
+];
+
+export const registerValidation = [
+    body("email", "Неправильний формат пошти").isEmail(),
+    body("password", "Пароль має бути імнімум 5 символів").isLength({ min: 5 }),
+    body("fullName", "Вкажіть ім'я").isLength({ min: 3 }),
+];
+
+
